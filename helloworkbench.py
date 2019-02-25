a5b105ef449f3e8567d40e6c38dd489f2aceb6ab
Adam Hooper <adam@adamhooper.com>
2019-02-25 11:45:05 -0500

    s/columns/input_columns/

--- helloworkbench.py
+++ helloworkbench.py
@@ -1,22 +1,19 @@
-from pandas.api.types import is_numeric_dtype
-
-
 def render(table, params, *, input_columns):
     colname = params['colname']
-    factor  = params['factor']
+    factor = params['factor']
 
-    # If no column is selected, do nothing. Workbench's convention is modules 
-    # do not alter their input when waiting for parameters to be filled out
+    # If no column is selected, do nothing. By convention, Workbench modules
+    # should output their input until enough parameters are supplied.
     if not colname:
         return table
 
     # Don't worry about table[colname] not existing. Workbench guarantees it
     # exists.
-    
+
     # If the column is not a number, return an error message
     # see https://github.com/CJWorkbench/cjworkbench/wiki/Column-Types
-    if columns[colname].type != 'number':
-    	return "Please select a Number column"
+    if input_columns[colname].type != 'number':
+        return "Please select a Number column"
 
     # Modules may alter their input in place, if desired
     table[colname] *= factor
